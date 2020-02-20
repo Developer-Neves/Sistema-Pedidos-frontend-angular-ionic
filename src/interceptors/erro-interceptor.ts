fe92a24a4a0a4a8ba8fa13859ca8800691932326
developerneves@gmail.com <developerneves@gmail.com>
2020-02-19 23:17:13 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/erro-interceptor.ts
+++ src/interceptors/erro-interceptor.ts
@@ -2,11 +2,15 @@ import { Injectable } from "@angular/core";
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageSercive } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageSercive){}
+    constructor(
+        public storage: StorageSercive,
+        public alertCtrl: AlertController,
+        ){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         //console.log('Passou no interceptor');
@@ -19,13 +23,19 @@ export class ErrorInterceptor implements HttpInterceptor {
                 errorObj = errorObj.error;
             }         
             
-            /* if(errorObj.status){
+            /*if(errorObj.status){
                 errorObj = JSON.parse(errorObj);
-            } */
+            }*/
 
             switch(errorObj.status){
+                case 401: 
+                    this.handle401();
+                    break;
                 case 403: 
                     this.handle403();
+                    break;
+                default:
+                    this.handleDefaultError(errorObj);
                     break;
             }
             
@@ -37,9 +47,30 @@ export class ErrorInterceptor implements HttpInterceptor {
         }) as any;
     }
 
-    handle403(){        
+    handle401(){    
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [{ text:'OK'}]
+        }) 
+        alert.present();
         this.storage.setLocalUser(null);
-        console.log('Passou no interceptor 3');
+    }
+
+    handle403(){ 
+        this.storage.setLocalUser(null);
+        console.log('Passou no handle403');
+    }
+
+    handleDefaultError(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [{ text:'OK'}]
+        }) 
+        alert.present();
     }
 }
 
