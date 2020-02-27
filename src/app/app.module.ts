5973deae4c2abd6aad9c1402409c24e6d96e88de
developerneves@gmail.com <developerneves@gmail.com>
2020-02-26 23:04:11 -0300

    Carregando produtos de uma dada categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AuthService } from '../services/auth.service';
 import { StorageSercive } from '../services/storage.service';
 import { ClienteService } from '../services/domain/cliente.service';
 import { AuthInterceptorProvider } from '../interceptors/auto-interceptor';
+import { ProdutoService } from '../services/domain/produto.service';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { AuthInterceptorProvider } from '../interceptors/auto-interceptor';
     AuthService,
     StorageSercive,
     ClienteService,
+    ProdutoService,
   ]
 })
 export class AppModule {}
--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { ProdutoService } from '../../services/domain/produto.service';
 
 @IonicPage()
 @Component({
@@ -14,22 +15,16 @@ export class ProdutosPage {
   
   constructor(
     public navCtrl: NavController, 
-    public navParams: NavParams) {
+    public navParams: NavParams,
+    public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-    {
-      id: '1',
-      nome: 'Mouse',
-      preco: 80.99
-    },
-    {
-      id: "2",
-      nome: 'Teclado',
-      preco: 100.00
-    }
-  ]
-  };
-
+    let categoria_id = this.navParams.get('cat_id');
+    this.produtoService.findByCategoria(categoria_id)
+      .subscribe(response => {
+        this.items = response['content'];
+      },
+      error => {});
+  }
 }
